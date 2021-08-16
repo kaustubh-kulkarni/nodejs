a5da526de159419d8dfc97df4ee0971649ce4e89
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-08-16 20:19:17 +0200

    Create another method to get specific post by id

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -9,7 +9,7 @@ const db = require("../config/database");
 //call model
 const Post = require("../models/Post");
 
-//Get request
+//Get all posts request
 router.get('/', (req, res) => 
         Post.findAll()
             .then(documents => {
@@ -20,6 +20,18 @@ router.get('/', (req, res) =>
             })
             .catch(err => console.log(err)));
 
+//Get specific post
+router.get('/:id', (req, res) => 
+            Post.findByPk(req.params.id)
+                .then(post => {
+                    if(post){
+                        res.status(200).json(post);
+                    } else {
+                        res.status(404).json({
+                            message: "Post not found! Please check the id"
+                        });
+                    }
+                }));
 
 
 //export it
